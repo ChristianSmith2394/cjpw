a4a1938a573affa15efb36020e3a896786a3b14d
Christian <christiansmith2394@gmail.com>
2024-06-30 21:35:05 -0400

    importing style sheets to components

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import "./reviews.css";
 
 const Reviews = () => {
-  return (
-    <div>Reviews</div>
-  )
-}
+  return <div>Reviews</div>;
+};
 
-export default Reviews
+export default Reviews;
